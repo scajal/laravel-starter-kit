ec5145ad8371edea3182dc0a9e306831ccd63324
Santiago Cajal <s.cajalvarela@gmail.com>
2026-01-05 14:21:03 -0300

    Add barryvdh/laravel-ide-helper as a development dependency in composer.json and update composer.lock. Refactor rector.php to include new PHP 8.3 features and clean up unused rules.

--- rector.php
+++ rector.php
@@ -2,50 +2,25 @@
 
 declare(strict_types=1);
 
-use Rector\Caching\ValueObject\Storage\FileCacheStorage;
-use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
-use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
-use Rector\CodeQuality\Rector\If_\CombineIfRector;
-use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
-use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
-use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
-use Rector\CodingStyle\Rector\If_\NullableCompareToNullRector;
-use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
 use Rector\Config\RectorConfig;
-use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
-    ->withCache(
-        cacheDirectory: '/tmp/rector',
-        cacheClass: FileCacheStorage::class,
-    )
     ->withPaths([
         __DIR__.'/app',
         __DIR__.'/bootstrap/app.php',
-        __DIR__.'/bootstrap/providers.php',
-        __DIR__.'/config',
         __DIR__.'/database',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
+        __DIR__.'/public',
     ])
     ->withSkip([
-        CombineIfRector::class,
-        DisallowedEmptyRuleFixerRector::class,
-        EncapsedStringsToSprintfRector::class,
-        ExplicitBoolCompareRector::class,
-        NewlineBeforeNewAssignSetRector::class,
-        NullableCompareToNullRector::class,
-        SeparateMultiUseImportsRector::class,
-        SimplifyEmptyArrayCheckRector::class,
-        SimplifyEmptyCheckOnEmptyArrayRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
     ])
     ->withPreparedSets(
+        deadCode: true,
         codeQuality: true,
         codingStyle: true,
-        deadCode: true,
-        earlyReturn: true,
-        instanceOf: true,
-        privatization: true,
         typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
     )
     ->withPhpSets();
